b51ef391416414c20bd5ed35ba702e9300913ad5
Luciano Carreiro Oliveira da Silva <luciano2911@gmail.com>
2021-01-26 00:07:40 -0300

    initial commit

---
+++ kafka-consumer-csharp/kafka-consumer-csharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace kafka_consumer_csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "127.0.0.1:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("test-topic");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ kafka-producer-csharp/kafka-producer-csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kafka_producer_csharp
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = $"test - {count++}" });
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+
+                        Thread.Sleep(2000);
+                    }
+          
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
